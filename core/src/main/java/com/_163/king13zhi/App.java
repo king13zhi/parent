da34f737c070a2508921d50b870475d349f901ad
king <king13zhi@163.com>
2018-02-05 20:47:47 +0800

    项目基本准备

---
+++ core/src/main/java/com/_163/king13zhi/App.java
@@ -0,0 +1,13 @@
+package com._163.king13zhi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
